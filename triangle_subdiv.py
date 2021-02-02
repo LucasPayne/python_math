36ae9b8c0886264cabf4cb0e6d9b3fe77570c14d
lucaspayne <lucaspayyne@gmail.com>
2021-02-02 18:48:42 +1300

    tedious linear system thing apparently worked

--- triangle_subdiv.py
+++ triangle_subdiv.py
@@ -24,46 +24,102 @@ def ordered_sums(terms, n):
         for trailing in ordered_sums(terms-1, n-i):
             yield tuple([i]) + trailing
 
+# Wrapper generator which gives a flat index as well.
+def ordered_sums_indexed(terms, n):
+    i = 0
+    for t in ordered_sums(terms, n):
+        yield tuple([i]) + t
+        i += 1
+
+
 
 def bezier_triangle_basis(degree):
     x,y,z = Sym("x y z")
     p = sym.poly((x + y + z)**degree)
     coeffs = []
-    for (i,j,k) in ordered_sums(3, degree):
+    for i,j,k in ordered_sums(3, degree):
         coeffs.append(p.coeff_monomial(x**i * y**j * z**k) * x**i * y**j * z**k)
     return coeffs
 
 
 
 
-degree = 2
-coeffs = bezier_triangle_basis(degree)
-x,y,z = Sym("x y z")
+# Given a triangular Bezier patch, there exist control points for the image of the middle triangle of the domain.
+# These control points are convex combinations of the control points for the patch.
+def triangular_bezier_subdivision_scheme(degree):
+    print("============================================================")
+    print("Triangular bezier patch interior triangle subdivision scheme")
+    print("    degree = {}".format(degree))
+    x,y,z = Sym("x y z")
+    
+    # An affine transformation of the parameter domain induces an affine transformation on the control points,
+    # which can be represented as a matrix where each row sums to 1.
+    # The weights in this matrix are computed by substituting the transformed x,y,z into the Bezier triangle polynomial and expanding, then
+    # equating coefficients of each of the basis polynomials.
+    ps = Sym(" ".join("p_{}{}{}".format(i,j,k) for i,j,k in ordered_sums(3, degree)))
+    print(ps)
+    q_poly = sym.poly(sum([(ps[index] * trinomial(degree, i,j,k) * ((x+y)/2)**i * ((y+z)/2)**j * ((z+x)/2)**k) for index,i,j,k in ordered_sums_indexed(3, degree)]))
+    
+    for i,j,k in ordered_sums(3, degree):
+        c = sum(point * q_poly.coeff_monomial(x**i * y**j * z**k * point) / trinomial(degree, i,j,k) for point in ps)
+        print("q_{}{}{} = {}".format(i,j,k, c))
+    
+
+for i in range(1,4):
+    triangular_bezier_subdivision_scheme(i)
 
 
 
-coeffs_q = [(Rat(1,2**degree) * trinomial(degree, i,j,k) * (x + z)**i * (x+y)**j * (x+z)**k).expand() for i,j,k in ordered_sums(3, degree)]
+def quadratic_triangular_bspline_subdivision_scheme():
+    X,Y,Z = Sym("X Y Z")
+    x,y,z = Sym("x y z")
 
-ps = Sym(" ".join("p_{}{}{}".format(i,j,k) for i,j,k in ordered_sums(3, degree)))
-q_poly = sum(ps[i]*coeffs_q[i] for i in range(len(coeffs_q))).as_poly()
+    basis = [
+	X**2 / 2,
+        Y**2 / 2,
+        Z**2 / 2,
+        X/2 + Y/2 + Z/2 + X*Y + X*Z + Y*Z
+    ]
 
-for i,j,k in ordered_sums(3, degree):
-    c = sum(point * q_poly.coeff_monomial(x**i * y**j * z**k * point**1) for point in ps)
-    print("q_{}{}{} = {}".format(i,j,k, c))
+    ps = Sym("p_0 p_1 p_2 p_3")
+    substituted_basis = [f.subs({
+        X: (y+z)/2,
+        Y: (x+z)/2,
+        Z: (x+y)/2,
+    }).expand() for f in basis]
+    q_poly = sum(ps[i] * substituted_basis[i] for i in range(4)).expand()
 
-# for i in range(len(coeffs_q)):
-#     print(q_poly.coeff(ps[i], 1))
-# 
-# for c in coeffs:
-#     print(c)
+    print(q_poly)
+    print("--------------------------------------------------------------------------------")
+    
+    print(2 * q_poly.coeff(x, 2))
+    print(2 * q_poly.coeff(y, 2))
+    print(2 * q_poly.coeff(z, 2))
+    f = x/2 + y/2 + z/2 + x*y + x*z + y*z
 
+    g = q_poly
+    g -= x**2 * q_poly.coeff(x, 2)
+    print(sym.div(q_poly, f))
+    g -= y**2 * q_poly.coeff(y, 2)
+    g -= z**2 * q_poly.coeff(z, 2)
+    g = g.expand()
+    print(f)
+    print(g)
 
-# M = Mat([[1,2,1,-2,-2,1],
-#          [-2,0,2,4,0,-2],
-#          [1,-2,1,-2,2,1],
-#          [2,0,-2,0,4,-2],
-#          [-2,4,-2,0,0,2],
-#          [1,-2,1,2,-2,1]])
-# print_matrix(M.inv())
+    for f in substituted_basis:
+        print(f)
 
-print_matrix(Mat([[Half, Half, 0], [0, Half, Half], [Half, 0, Half]]).inv())
+    
+    
+quadratic_triangular_bspline_subdivision_scheme()
+
+A = Mat([[0,0,0,Half,0,0,0,0,0],
+         [0,0,0,0,Half,0,0,0,0],
+         [0,0,0,0,0,Half,0,0,0],
+         [Half,Half,Half,0,0,0,1,1,1]])
+AM = Mat([[0,0,0,0,Rat(1,8),Rat(1,8),0,0,Quarter],
+          [0,0,0,Rat(1,8),0,Rat(1,8),0,Quarter,0],
+          [0,0,0,Rat(1,8),Rat(1,8),0,Quarter,0,0],
+          [Half,Half,Half,Quarter,Quarter,Quarter,Rat(3,4),Rat(3,4),Rat(3,4)]])
+print_matrix(AM * A.T * (A * A.T).inv())
+
