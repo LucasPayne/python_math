ed2dae2371f97274aa1fd762bd0a5dc4f1e635a8
lucaspayne <lucaspayyne@gmail.com>
2021-02-06 20:53:04 +1300

    converting multiset indices to tensor indices and flat packed indices with some arithmetic

--- triangle_subdiv.py
+++ triangle_subdiv.py
@@ -214,98 +214,142 @@ bezier_triangle_subpatch(2,
 # bezier_triangle_bottom_left_subpatch(3)
 
 
+from math import factorial
 
-def rising_factorial
+def rising_factorial(k, n):
+    t = 1
+    for i in range(n):
+        t *= k + i
+    return t
 
-def symmetric_tensor_size(k, rank):
+def stensor_size(k, rank):
     # The size is the k'th rank-figurate number.
     # (e.g. 2-figurate numbers are triangular, 3-figurate are pyramidal, etc.)
     return rising_factorial(k, rank) // factorial(rank)
-    
 
-from math import factorial
-class symmetric_tensor:
-    def __init__(self, k, rank, init_array=None)
+
+def stensor_indices(k, rank):
+    if k == 1:
+        yield tuple([rank])
+        return
+    for i in range(0, rank+1):
+        for trailing in stensor_indices(k-1, rank-i):
+            yield tuple([i]) + trailing
+
+def stensor_indices_size(k, rank):
+    if k == 1:
+        return 1
+    return sum(stensor_indices_size(k-1, rank-i) for i in range(0, rank+1))
+
+
+class stensor:
+    def __init__(self, k, rank, init_array=None):
         if init_array == None:
             init_array = []
-            self._vals = [0 for _ symmetric_tensor_size(k, rank)]
-        assert(len(init_array) == symmetric_tensor_size(k, rank))
-        self._vals = init_array
+            self._vals = [0 for _ in range(stensor_size(k, rank))]
+        else:
+            assert(len(init_array) == stensor_size(k, rank))
+            self._vals = init_array
         self.k = k
         self.rank = rank
 
+
+    def to_tensor_index(self, index):
+        assert(len(index) == self.k)
+        assert(sum(index) == self.rank)
+        tensor_index = [0 for _ in range(self.rank)]
+        pos = 0
+        for basis_index, multiplicity in enumerate(index):
+            for i in range(multiplicity):
+                tensor_index[pos + i] = basis_index
+            pos += multiplicity
+        return tensor_index
+
     def __getitem__(self, index):
-        assert(len(index) == rank
+        tensor_index = self.to_tensor_index(index)
+        flat_index = 0
+        for position, i in enumerate(tensor_index):
+            flat_index += rising_factorial(self.k-1-i, self.rank - position) // factorial(self.rank - position)
+        # return _vals[flat_index]
+        return flat_index
     
 
 
-def bezier_triangle_subpatch_2(degree, M):
-    # degree: Degree of the the polynomial map. Determines the rank of the symmetric tensor and therefore the number of control points in the net.
-    k = 3 # Number of points in a basis simplex of the affine domain.
-    # M: (k+1)x(k+1) affine domain transformation matrix acting as Mp on points. Columns must sum to 1.
-
-
-    rank = degree # Rank of the symmetric tensor representing the polar form of the polynomial map.
-
-    # Form the symmetric tensor of symbolic control points.
-    tensor_vars = sym.symbols(" ".join("N_" + "".join(str(i) for i in index)) for index in itertools.product(k, repeat=rank))
-
-
-
-    control_point_vars = symmetric_tensor(k, rank,
-        sym.symbols(" ".join("P_" + "".join(i) for i in index) for index in ordered_sums(k, rank)
-    )
-
-    control_point_vars = symmetric_tensor(sym.symbols(" ".join("P_" + "".join(i) for i in index) for index in ordered_sums(k+1, rank)))
-
-
-
-
-    # tensor_weights = [[0 for _ in range(degree+1)] for __ in range(degree+1)]
-    # tensor_weights = [0 for _ in range((k+1)**rank)]
-
-
-
-    for i,j in itertools.product(range(degree+1), repeat=2):
-        tensor_weights[i][j] = sum(tensor_vars[ii][jj] * M[ii, i] * M[jj, j] for ii,jj in itertools.product(range(degree+1), repeat=2))
-
-
-    mat = sym.Matrix(tensor_weights)
-    print_matrix(mat)
-
-    for i,j in itertools.product(range(degree+1), repeat=2):
-        tri_index = [0,0,0]
-        tri_index[i] += 1
-        tri_index[j] += 1
-        net_var = sym.symbols("P_" + "".join(str(k) for k in tri_index))
-        mat = mat.subs(tensor_vars[i][j], net_var)
-    print_matrix(mat, lower_triangular=True)
+# def bezier_triangle_subpatch_2(degree, M):
+#     # degree: Degree of the the polynomial map. Determines the rank of the symmetric tensor and therefore the number of control points in the net.
+#     k = 3 # Number of points in a basis simplex of the affine domain.
+#     # M: (k+1)x(k+1) affine domain transformation matrix acting as Mp on points. Columns must sum to 1.
+# 
+# 
+#     rank = degree # Rank of the symmetric tensor representing the polar form of the polynomial map.
+# 
+#     # Form the symmetric tensor of symbolic control points.
+#     tensor_vars = sym.symbols(" ".join("N_" + "".join(str(i) for i in index)) for index in itertools.product(k, repeat=rank))
+# 
+# 
+# 
+#     control_point_vars = symmetric_tensor(k, rank,
+#         sym.symbols(" ".join("P_" + "".join(i) for i in index) for index in ordered_sums(k, rank)
+#     )
+# 
+#     # control_point_vars = symmetric_tensor(sym.symbols(" ".join("P_" + "".join(i) for i in index) for index in ordered_sums(k+1, rank)))
+# 
+# 
+# 
+# 
+#     # tensor_weights = [[0 for _ in range(degree+1)] for __ in range(degree+1)]
+#     # tensor_weights = [0 for _ in range((k+1)**rank)]
+# 
+# 
+# 
+#     for i,j in itertools.product(range(degree+1), repeat=2):
+#         tensor_weights[i][j] = sum(tensor_vars[ii][jj] * M[ii, i] * M[jj, j] for ii,jj in itertools.product(range(degree+1), repeat=2))
+# 
+# 
+#     mat = sym.Matrix(tensor_weights)
+#     print_matrix(mat)
+# 
+#     for i,j in itertools.product(range(degree+1), repeat=2):
+#         tri_index = [0,0,0]
+#         tri_index[i] += 1
+#         tri_index[j] += 1
+#         net_var = sym.symbols("P_" + "".join(str(k) for k in tri_index))
+#         mat = mat.subs(tensor_vars[i][j], net_var)
+#     print_matrix(mat, lower_triangular=True)
         
         
 
-for degree in [2,3]:
-    print("Identity")
-    bezier_triangle_subpatch_2(degree, sym.eye(3))
-    print("Lower-left isometric")
-    bezier_triangle_subpatch_2(degree, Mat([
-        [1, Half, Half],
-        [0, Half, 0],
-        [0, 0,    Half]
-    ]))
-    print("Middle isometric")
-    bezier_triangle_subpatch_2(degree, Mat([
-        [Rat(1,2), Rat(1,2), 0],
-        [0,        Rat(1,2), Rat(1,2)],
-        [Rat(1,2), 0,        Rat(1,2)]
-    ]))
+# for degree in [2,3]:
+#     print("Identity")
+#     bezier_triangle_subpatch_2(degree, sym.eye(3))
+#     print("Lower-left isometric")
+#     bezier_triangle_subpatch_2(degree, Mat([
+#         [1, Half, Half],
+#         [0, Half, 0],
+#         [0, 0,    Half]
+#     ]))
+#     print("Middle isometric")
+#     bezier_triangle_subpatch_2(degree, Mat([
+#         [Rat(1,2), Rat(1,2), 0],
+#         [0,        Rat(1,2), Rat(1,2)],
+#         [Rat(1,2), 0,        Rat(1,2)]
+#     ]))
 
 
 
+for index in stensor_indices(3, 2):
+    print(index)
 
-# def bezier_simplex_subpatch(M):
-#     N = [[Sym("N_{}{}".format(i, j)) for j in range(3)] for i in range(3)]
-#     mat = sym.zeros(3,3)
-#     for i,j in itertools.product(range(3), repeat=2):
-#         mat += N[i][j] * (M.row(i).T * M.row(j))
-#     print_matrix(mat.subs())
+print("---")
+for index in stensor_indices(3, 3):
+    print(index)
+print(stensor_indices_size(3,3))
 
+
+k = 3
+r = 4
+T = stensor(k, r)
+for index in stensor_indices(k, r):
+    print(index, T.to_tensor_index(index))
+for index in stensor_indices(k, r):
+    print(T[index])
