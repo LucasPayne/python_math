bca32edd762b5429ad8cdec3049cbc19aeec738a
lucaspayne <lucaspayyne@gmail.com>
2021-09-18 18:32:29 +1200

    math

--- triangles.py
+++ triangles.py
@@ -7,7 +7,6 @@ def side_length(p, q):
 
 def draw_circle(c, radius):
     ps = [(c[0] + radius*cos(theta), c[1] + radius*sin(theta)) for theta in np.linspace(0,2*pi,100)]
-    print(ps)
     plt.plot(*zip(*ps))
 
 
@@ -16,15 +15,25 @@ def draw_tri(p, q, r):
     a = np.linalg.norm(q - r)
     b = np.linalg.norm(r - p)
     c = np.linalg.norm(p - q)
-    c = (a*p + b*q + c*r)/(a + b + c)
 
-    plt.scatter([c[0]], [c[1]])
+    center = (a*p + b*q + c*r)/(a + b + c)
+    plt.scatter([center[0]], [center[1]])
+    l = np.dot(center - p, (q-p)/np.linalg.norm(q-p))
+    d = sqrt(np.dot(center - p, center - p) - l**2)
+    draw_circle(center, d)
 
-    l = np.dot(c - p, (q-p)/np.linalg.norm(q-p))
-    d = sqrt(np.dot(c - p, c - p) - l**2)
-    print(d)
+    r1 = np.array([p[1] - q[1], q[0] - p[0]])
+    h1 = np.array([*((p+q)/2), 1])
 
-    draw_circle(c, d)
+    r2 = np.array([p[1] - r[1], r[0] - p[0]])
+    h2 = np.array([*((p+r)/2), 1])
+
+    point = np.cross(np.cross(r1, h1), np.cross(r2, h2))
+    point = point[:2]/point[2]
+    plt.scatter(*point)
+    radius = np.linalg.norm(point - p)
+    draw_circle(point, radius)
+    
 
 
 draw_tri(np.array([0,0]), np.array([2,0]), np.array([3,2]))
